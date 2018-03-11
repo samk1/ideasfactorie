63ee26a0f13a3f29234d4701f8eebc915eb2f2dc
Sam Kidman <samuel@netwealth.com.au>
2018-03-11 11:57:48 +1100

    initial commit

---
+++ ios/src/net/ideasfactorie/games/sockgame/IOSLauncher.java
@@ -0,0 +1,22 @@
+package net.ideasfactorie.games.sockgame;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import net.ideasfactorie.games.sockgame.SockGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new SockGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
